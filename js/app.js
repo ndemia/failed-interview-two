861aa2b2a1e27c72d30be64f98adc340d80dc7df
Nico <info@demia.me>
2022-08-08 17:22:16 +0200

    Added basic market operations (add and remove items, show total)

--- js/app.js
+++ js/app.js
@@ -14,11 +14,11 @@
 // });
 
 
-// Show name and gold balance
+///// Show name and gold balance
 document.getElementById('gold-balance').innerText = `${service.user.balance} gold`;
 document.getElementById('user-name').innerText = `${service.user.login}`;
 
-
+///// Show current stock
 // UI variables
 const container = document.getElementById('stock');
 const productsList = document.querySelector('.products__list');
@@ -34,20 +34,20 @@ currentStock.forEach(item => {
         </div>
     </li>`);
     
-    //Load the market as if it was not defined in the HTML
-    // productsList.insertAdjacentHTML('afterbegin',
-    // `<li class="products__item">
-    //     <div class="item__container">
-    //         <img src="images/${item.filename}.png" class="item__image">
-    //         <h4 class="item__name">${item.name}</h4>
-    //         <div class="item__actions">
-    //             <button class="item__decrease"><img src="icons/minus.png"></button>
-    //             <input type="number" name="item__quantity" class="item__quantity">
-    //             <button class="item__increase"><img src="icons/plus.png"></button>	
-    //             <span class="item__cost">${item.price} gold</span>
-    //         </div>
-    //     </div>
-    // </li>`);
+    // Load the market as if it was not defined in the HTML
+    productsList.insertAdjacentHTML('beforeend',
+    `<li class="products__item">
+        <div class="item__container">
+            <img src="images/${item.filename}.png" class="item__image">
+            <h4 class="item__name">${item.name}</h4>
+            <div data-item-id="${item.id}" class="item__actions">
+                <button class="item__decrease">-</button>
+                <input type="number" name="item__quantity" class="item__quantity">
+                <button class="item__increase">+</button>	
+                <span class="item__cost">0 gold</span>
+            </div>
+        </div>
+    </li>`);
 });
 
 
@@ -57,7 +57,6 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.querySelector('.overlay');
 
-// Functions made with the idea of more modals being present
 const openModal = function(modal) {
 
     if (modal == null) {
@@ -92,7 +91,6 @@ closeModalButtons.forEach(button => {
 
     button.addEventListener('click', () => {
 
-        // Detect, from the clicked button, which modal will be closed 
         const modal = button.closest('.modal');
         closeModal(modal);
     })
@@ -101,7 +99,6 @@ closeModalButtons.forEach(button => {
 // Close the modal by clicking the overlay
 overlay.addEventListener('click', () => {
 
-    // Detect the active modal to close it
     const modals = document.querySelectorAll('.modal.active');
     modals.forEach(modal => {
         closeModal(modal);
@@ -114,24 +111,85 @@ overlay.addEventListener('click', () => {
 const quantityIncreaseButtons = document.querySelectorAll('.item__increase');
 const quantityDecreaseButtons = document.querySelectorAll('.item__decrease');
 
+const updateItemCost = function(itemQuantity, itemId) {
+
+    // Because itemId arrives as a string from the object
+    itemId = Number(itemId)
+
+    let updatedPrice;
+
+    currentStock.forEach(item => {
+        if(item.id === itemId) {
+            updatedPrice = itemQuantity * item.price;
+        }
+    });
+
+    return updatedPrice;
+};
+
+const updateTotalCost = function() {
+
+    let totalCost = 0;
+    
+    document.querySelectorAll('.item__cost').forEach(cost => {
+
+        totalCost += Number(cost.innerText.slice(0, -5));
+
+    })
+
+    document.querySelector('.total__value').innerText = `${totalCost} gold`;
+
+};
+
 quantityIncreaseButtons.forEach(button => {
 
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
 
-        button.previousElementSibling.value++;
-    })
+        // Identify and save the pressed button in order to have a better context of where and which inputs/values to update
+        const pressedButton = e.target;
+
+        // Save the item's ID to find the price
+        const itemId = pressedButton.parentElement.dataset.itemId;
+      
+        pressedButton.previousElementSibling.value++;
+
+        // Save the newly updated item quantity
+        let itemQuantity = pressedButton.previousElementSibling.value;
+
+        let totalItemCost = updateItemCost(itemQuantity, itemId);
+
+        pressedButton.nextElementSibling.innerText = `${totalItemCost} gold`;
+
+        updateTotalCost();
+
+    });
 
 })
 
 quantityDecreaseButtons.forEach(button => {
 
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
+
+        // Identify and save the pressed button in order to have a better context of where and which inputs/values to update
+        const pressedButton = e.target;
+
+        // Save the item's ID to find the price
+        const itemId = pressedButton.parentElement.dataset.itemId;
 
         if (button.nextElementSibling.value <= 0) {
             return;
         } else {
             button.nextElementSibling.value--;
         }
-    })
+
+        // Save the newly updated item quantity
+        let itemQuantity = pressedButton.nextElementSibling.value;
+
+        let totalItemCost = updateItemCost(itemQuantity, itemId);
+
+        pressedButton.nextElementSibling.nextElementSibling.nextElementSibling.innerText = `${totalItemCost} gold`;
+
+        updateTotalCost();
+    });
 
 })
