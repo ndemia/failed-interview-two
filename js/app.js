c25bb218e66111c3ecbe9542f25d3ad7339879c3
ndemia <info@demia.me>
2023-05-12 15:23:53 +0200

    Cleanup of main ts file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 // Original test code
-// $(function() {
-//     service.getUser()
-//         .then(function (user) {
-//             $("#user-name").text(user.login);
-//             $("#gold-balance").text(user.balance);
-//         })
-//         .then(service.list)
-//         .then(function(items) {
-//             var container = $("#stock");
-//             items.forEach(function (item) {
-//                 container.append(item.name + ": " + item.quantity + "<br/>");
-//             });
-//         });
+// $(function () {
+// 	service.getUser()
+// 		.then(function (user) {
+// 			$("#user-name").text(user.login);
+// 			$("#gold-balance").text(user.balance);
+// 		})
+// 		.then(service.list)
+// 		.then(function (items) {
+// 			var container = $("#stock");
+// 			items.forEach(function (item) {
+// 				container.append(item.name + ": " + item.quantity + "<br/>");
+// 			});
+// 		});
 // });
 import { Service } from './service.js';
 import './inert.min.js';
@@ -30,28 +30,27 @@ const showUserName = () => {
 const showCurrentStock = () => {
     currentStock.forEach(item => {
         container.insertAdjacentHTML('beforeend', `<li class="dashboard__item">
-            <img src="images/${item.filename}.png" class="item__image" alt="A ${item.name}"/>
-            <div class="item__info">
-                <h3 class="item__name">${item.name}</h3>
-                <p class="item__quantity" data-item-id="${item.id}">Quantity: ${item.quantity}</p>
-                <p class="item__price" data-item-id="${item.id}">Price: ${item.price} gold</p>
-            </div>
-        </li>`);
-        // Load the market as if it was not defined in the HTML
+				<img src="images/${item.filename}.png" class="item__image" alt="A ${item.name}"/>
+				<div class="item__info">
+					<h3 class="item__name">${item.name}</h3>
+					<p class="item__quantity" data-item-id="${item.id}">Quantity: ${item.quantity}</p>
+					<p class="item__price" data-item-id="${item.id}">Price: ${item.price} gold</p>
+				</div>
+			</li>`);
         productsList.insertAdjacentHTML('beforeend', `<li class="products__item">
-            <div class="item__container">
-                <div class="item__info">
-                    <img src="images/${item.filename}.png" class="item__image" alt="A ${item.name}">
-                    <p class="item__name">${item.name}</p>
-                </div>
-                <div class="item__actions" data-item-id="${item.id}">
-                    <button class="item__decrease" aria-label="Decrease ${item.name} quantity">-</button>
-                    <input type="number" name="item__quantity" class="item__quantity" value="0" min="0" max="${item.quantity}" data-item-id="${item.id}" aria-label="${item.name} quantity" role="status">
-                    <button class="item__increase" aria-label="Increase ${item.name} quantity">+</button>	
-                </div>
-                <span class="item__cost" role="status">0 gold</span>
-            </div>
-        </li>`);
+			<div class="item__container">
+				<div class="item__info">
+					<img src="images/${item.filename}.png" class="item__image" alt="A ${item.name}">
+					<p class="item__name">${item.name}</p>
+				</div>
+				<div class="item__actions" data-item-id="${item.id}">
+					<button class="item__decrease" aria-label="Decrease ${item.name} quantity">-</button>
+					<input type="number" name="item__quantity" class="item__quantity" value="0" min="0" max="${item.quantity}" data-item-id="${item.id}" aria-label="${item.name} quantity" role="status">
+					<button class="item__increase" aria-label="Increase ${item.name} quantity">+</button>	
+				</div>
+				<span class="item__cost" role="status">0 gold</span>
+			</div>
+		</li>`);
     });
 };
 document.addEventListener('DOMContentLoaded', showUserName);
