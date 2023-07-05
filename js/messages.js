5e42347353a92d624bd1b98e7fe6cf7d314e0a23
ndemia <ndemia@fastmail.com>
2023-07-05 15:31:47 +0200

    Fix successful purchase message showing 2 icons

--- js/messages.js
+++ js/messages.js
@@ -32,15 +32,14 @@ export const showMessage = (messageType, itemName) => {
             dashboardMessageContainer.classList.remove('hidden');
             dashboardMessage.classList.add('warning--secondary');
             dashboardMessageText.innerText = `Your purchase was successful.`;
-            iconContainer.insertAdjacentHTML('afterbegin', `<svg viewBox="0 0 24 24" class="message__icon">
+            iconContainer.innerHTML = `<svg viewBox="0 0 24 24" class="message__icon">
 					<path d="M20.521,3.79l-8.229-2.743c-0.19-0.063-0.396-0.063-0.586,0L3.479,3.79C3.09,3.92,2.836,4.294,2.857,4.705v5.952
 					c-0.008,5.577,3.559,10.529,8.85,12.289c0.096,0.028,0.193,0.044,0.293,0.046c0.098,0.014,0.195,0.014,0.293-0.001
 					c5.307-1.764,8.878-6.74,8.85-12.333V4.705C21.165,4.293,20.911,3.92,20.521,3.79z M17.323,9.909l-5.484,5.487
 					c-0.173,0.172-0.406,0.267-0.649,0.265c-0.244,0.002-0.477-0.093-0.648-0.265l-2.744-2.743c-0.358-0.359-0.358-0.941,0-1.298
 					c0.359-0.358,0.939-0.358,1.299,0l2.094,2.103l4.835-4.847c0.359-0.358,0.94-0.358,1.299,0C17.681,8.968,17.681,9.549,17.323,9.909z
-					"
-					/>
-				</svg>`);
+					" />
+				</svg>`;
             break;
         default:
             marketMessageContainer.classList.remove('hidden');
